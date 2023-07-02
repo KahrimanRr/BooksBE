9761c493d07adbec1f979631671e6ba3a3f22979
Rijad Kahriman <rijadkahr@gmail.com>
2023-07-02 19:03:58 +0200

    added entity and repo

---
+++ spring-boot-library/src/main/java/com/spring/springbootlibrary/dao/BookRepository.java
@@ -0,0 +1,7 @@
+package com.spring.springbootlibrary.dao;
+
+import com.spring.springbootlibrary.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
