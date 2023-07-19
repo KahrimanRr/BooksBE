39da79e21824f23e4264a5f4587d4e6753896782
Rijad Kahriman <rkahriman27@gmail.com>
2023-07-20 01:57:45 +0200

    added depen, modified properties still there is issue

--- spring-boot-library/src/main/java/com/spring/springbootlibrary/CONFIG/SecurityConfiguration.java
+++ spring-boot-library/src/main/java/com/spring/springbootlibrary/CONFIG/SecurityConfiguration.java
@@ -12,27 +12,31 @@ import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 @Configuration
 public class SecurityConfiguration {
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         // Disable Cross site request forgery
-      http.csrf().disable();
+        http.csrf().disable();
         // protect endpoints at /api/<type>/secure
-       http.authorizeRequests(configurer ->
-               configurer.antMatchers(HttpMethod.valueOf("/api/books/secure/**"))
-                       .authenticated())
-               .oauth2ResourceServer()
-               .jwt();
-       //add cors filters
+        http.authorizeRequests(configurer ->
+                        configurer.antMatchers(HttpMethod.valueOf("/api/books/secure/**"))
+                                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
+        //add cors filters
         http.cors();
 
         //add content negotiation strategy
         http.setSharedObject(ContentNegotiationStrategy.class,
                 new HeaderContentNegotiationStrategy());
 
-       //Force nonempty response body for 401 s to make response friendly
+        //Force nonempty response body for 401 s to make response friendly
         Okta.configureResourceServer401ResponseBody(http);
 
 
         return http.build();
     }
+
+
+
 }
