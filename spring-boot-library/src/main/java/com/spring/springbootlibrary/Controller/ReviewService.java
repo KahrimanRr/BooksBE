2ce076aa675ba4a75fdd120d2ba0ee171508ae92
Rijad Kahriman <rkahriman27@gmail.com>
2023-07-25 18:27:01 +0200

    created postreview in revievservice

---
+++ spring-boot-library/src/main/java/com/spring/springbootlibrary/Controller/ReviewService.java
@@ -0,0 +1,44 @@
+package com.spring.springbootlibrary.Controller;
+
+import com.spring.springbootlibrary.dao.BookRepository;
+import com.spring.springbootlibrary.dao.ReviewRepository;
+import com.spring.springbootlibrary.entity.Review;
+import com.spring.springbootlibrary.requestmodels.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+@Transactional
+public class ReviewService {
+    private BookRepository bookRepository;
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(BookRepository bookRepository, ReviewRepository reviewRepository) {
+        this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
+        var validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, reviewRequest.getBookId());
+        if(validateReview !=null){
+            throw new Exception("Review already created");
+        }
+        var review = new Review();
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(reviewRequest.getRating());
+        review.setUserEmail(userEmail);
+        if(reviewRequest.getReviewDescription().isPresent()){
+            review.setReviewDescription(reviewRequest.getReviewDescription().map(
+                    Object::toString
+            ).orElse(null));
+        }
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+
+    }
+}
