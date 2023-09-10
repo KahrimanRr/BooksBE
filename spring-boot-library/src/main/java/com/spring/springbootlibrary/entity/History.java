247227e4ccf7fcba7471ebb424b98b66803169e6
Rijad Kahriman <rkahriman27@gmail.com>
2023-09-10 21:40:20 +0200

    fixed minor issue in History entity

--- spring-boot-library/src/main/java/com/spring/springbootlibrary/entity/History.java
+++ spring-boot-library/src/main/java/com/spring/springbootlibrary/entity/History.java
@@ -12,35 +12,36 @@ import javax.persistence.*;
 public class History{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "user_email")
+    @Column(name="user_email")
     private String userEmail;
 
-    @Column(name = "checkout_date")
+    @Column(name="checkout_date")
     private String checkoutDate;
 
-    @Column(name = "return_date")
-    private String returnDate;
+    @Column(name="returned_date")
+    private String returnedDate;
 
-    @Column(name = "title")
+    @Column(name="title")
     private String title;
 
-    @Column(name = "author")
+    @Column(name="author")
     private String author;
 
-    @Column(name = "description")
+    @Column(name="description")
     private String description;
 
-    @Column(name = "img")
+    @Column(name="img")
     private String img;
 
-    public History(String userEmail, String checkoutDate, String returnDate, String title, String author, String description, String img) {
+    public History(String userEmail, String checkoutDate, String returnedDate, String title,
+                   String author, String description, String img) {
         this.userEmail = userEmail;
         this.checkoutDate = checkoutDate;
-        this.returnDate = returnDate;
+        this.returnedDate = returnedDate;
         this.title = title;
         this.author = author;
         this.description = description;
